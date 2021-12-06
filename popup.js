fa204e4e6fb4b008778c88b96b622023d3f6528b
Maxwell <hex0@live.com>
2021-12-06 22:18:19 +0000

    change background colour

--- popup.js
+++ popup.js
@@ -1,5 +1,18 @@
-let changeColor = document.getElementById("changeColor");
+// When the button is clicked, inject setPageBackgroundColor into current page
+changeColor.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: setPageBackgroundColor,
+  });
 });
+
+// The body of this function will be executed as a content script inside the
+// current page
+function setPageBackgroundColor() {
+  chrome.storage.sync.get("color", ({ color }) => {
+    console.log(color);
+    document.body.style.backgroundColor = color;
+  });
+}
